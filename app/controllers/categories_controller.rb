03b36e6b2495f1f42a3178ca05bf1a83ac5c6ca0
Guilherme Simoes <guilherme.rdems@gmail.com>
2017-05-27 18:38:11 +0100

    Create categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,56 @@
+class CategoriesController < ApplicationController
+  # GET /categories
+  def index
+    @categories = current_user.categories
+  end
+
+  # GET /categories/1
+  def show
+    @category = current_user.categories.find(params[:id])
+  end
+
+  # GET /categories/new
+  def new
+    @category = Category.new
+  end
+
+  # GET /categories/1/edit
+  def edit
+    @category = current_user.categories.find(params[:id])
+  end
+
+  # POST /categories
+  def create
+    @category = current_user.categories.new(category_params)
+
+    if @category.save
+      redirect_to categories_url, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /categories/1
+  def update
+    @category = current_user.categories.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_url, notice: 'Category was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /categories/1
+  def destroy
+    @category = current_user.categories.find(params[:id])
+    @category.destroy
+    redirect_to categories_url, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def category_params
+    params.require(:category).permit(:user_id, :name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   end
 
   resources :home, only: :index
+  resources :categories
 
   root to: 'home#index'
 end
