25ece27e954a3b205af20de4ae56bcf68e5750fb
Guilherme Simoes <guilherme.rdems@gmail.com>
2017-07-20 19:33:08 +0100

    Improve form styles

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,6 +4,6 @@ class Expense < ApplicationRecord
 
   attribute :created_at, :date, default: -> { Date.current }
 
-  validates :amount, :description, :categories, presence: true
+  validates :amount, :description, :category_ids, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
