06c317c86077a232db357125176e27aac2fb4249
Guilherme Simoes <guilherme.rdems@gmail.com>
2017-10-03 17:57:18 +0100

    Remove unnecessary default attribute

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,8 +2,6 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories
 
-  attribute :created_at, :date, default: -> { Date.current }
-
   validates :amount, :description, :category_ids, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
